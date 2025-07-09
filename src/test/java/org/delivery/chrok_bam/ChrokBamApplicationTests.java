4a6e191ff0bd040b7d78227b0134dee723737a5b
바카로이 <haeun8520@naver.com>
2025-07-09 16:30:03 +0900

    첫번째 커밋

---
+++ src/test/java/org/delivery/chrok_bam/ChrokBamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.delivery.chrok_bam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChrokBamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
