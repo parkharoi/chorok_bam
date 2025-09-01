db8f8732be623453e06ec3236786ac14022f5e5d
바카로이 <haeun8520@naver.com>
2025-09-01 21:42:34 +0900

    회원가입 로그인 완료

--- user-module/src/main/java/org/delivery/user/domain/service/UserService.java
+++ user-module/src/main/java/org/delivery/user/domain/service/UserService.java
@@ -1,7 +1,10 @@
 package org.delivery.user.domain.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.delivery.user.domain.User;
+import org.delivery.user.domain.UserRole;
+import org.delivery.user.domain.UserStatus;
 import org.delivery.user.domain.dto.UserLoginDto;
 import org.delivery.user.domain.dto.UserRegisterDto;
 import org.delivery.user.domain.repository.UserRepository;
@@ -9,8 +12,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
@@ -28,6 +34,9 @@ public class UserService {
                 .email(userRegisterDto.getEmail())
                 .password(encodedPassword)
                 .name(userRegisterDto.getName())
+                .status(UserStatus.REGISTERED)
+                .role(UserRole.USER)
+                .registeredAt(LocalDateTime.now())
                 .build();
 
         return userRepository.save(newUser);
@@ -39,6 +48,7 @@ public class UserService {
                 .orElseThrow(()-> new IllegalArgumentException("존재하지 않는 사용자입니다."));
 
         if (!passwordEncoder.matches(userLoginDto.getPassword(), user.getPassword())) {
+            log.warn("비밀번호 불일치: 이메일 = {}", userLoginDto.getEmail());
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
 
